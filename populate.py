2c597bdae4cc3037b73a1fc7eeb7ccc468227b14
Aleksander <aleksander.vestlund@hotmail.com>
2024-03-02 22:48:58 +0100

    Refactor main function and add query_user function

--- populate.py
+++ populate.py
@@ -160,11 +160,32 @@ class DatabaseCreator:
         self.con.close()
 
 
+def query_user(db: DatabaseCreator, query: bool = True) -> None:
+    """Spør brukeren hvor utfylt database de ønsker. Alle svar annet enn
+    1 og 2 vil gi en komplett database.
+
+    :param bool query: Om brukeren skal bli spurt, defaulter til True
+    """
+    print("Mulighet 1: Lag en tom database.")
+    print("Mulighet 2: Lag en database med tomme tabeller.")
+    print("Mulighet 3: Lag en database fylt med rader.")
+    option = input("Hvilken mulighet ønsker du? [1/2/3]\n") if query else "3"
+
+    if option == "1":
+        return
+    db.create_tables()
+    if option == "2":
+        return
+    db.fill_tables()
+
+
 def main() -> None:
     db = DatabaseCreator()
-    db.create_tables()
-    db.fill_tables()
-    db.print_all_tables(["Dato", "Stol", "Billett"])
+    query_user(db, True)
+    try:
+        db.print_all_tables(["Dato", "Stol", "Billett"])
+    except sqlite3.OperationalError:
+        print("Ingen tabeller.")
     db.close()
 
 
